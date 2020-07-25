b77a2d230d87e5d738aa625f07d18ff8e1b208e8
Shayne Hartford (ShayBox) <shaybox@shaybox.com>
2020-07-24 21:47:24 -0400

    Fabric 1.15 & 1.16

---
+++ Fabric-1.15/src/main/java/com/shaybox/durability101/CustomItemRenderer.java
@@ -0,0 +1,57 @@
+package com.shaybox.durability101;
+
+import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.Tessellator;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.MathHelper;
+
+import java.text.DecimalFormat;
+
+public class CustomItemRenderer {
+	public static void renderDurability(TextRenderer tr, ItemStack stack, int xPosition, int yPosition) {
+		if (!stack.isEmpty()) {
+			if (stack.isDamaged()) {
+				// Unbreaking
+				int unbreaking = EnchantmentHelper.getLevel(Enchantments.UNBREAKING, stack);
+
+				// Damage
+				float damage = (float) stack.getDamage();
+				float maxDamage = (float) stack.getMaxDamage();
+
+				// String
+				String string = format(((maxDamage - damage) * (unbreaking + 1)));
+				int stringWidth = tr.getStringWidth(string);
+
+				// Position
+				int x = ((xPosition + 8) * 2 + 1 + stringWidth / 2 - stringWidth);
+				int y = (yPosition * 2) + 18;
+
+				// Color
+				float hue = Math.max(0.0F, (maxDamage - damage) / maxDamage);
+				int color = MathHelper.hsvToRgb(hue / 3.0F, 1.0F, 1.0F);
+
+				// Draw
+				MatrixStack matrixStack = new MatrixStack();
+				matrixStack.translate(0.0D, 0.0D, 500.0F);
+				matrixStack.scale(0.5F, 0.5F, 0.5F);
+				VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
+				tr.draw(string, x, y, color, true, matrixStack.peek().getModel(), immediate, false, 0, 15728880);
+				immediate.draw();
+			}
+		}
+	}
+
+	private static String format(float number) {
+		DecimalFormat decimalFormat = new DecimalFormat("0.#");
+
+		if (number >= 1000000000) return decimalFormat.format(number / 1000000000) + "b";
+		if (number >= 1000000) return decimalFormat.format(number / 1000000) + "m";
+		if (number >= 1000) return decimalFormat.format(number / 1000) + "k";
+
+		return Float.toString(number).replaceAll("\\.?0*$", "");
+	}
+}
