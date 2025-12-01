681a0d4a69a93318334b4572aaa54fa40edd9693
ChooseC <3464861631@qq.com>
2025-12-01 22:09:28 +0800

    Modify the dependencies in version 1.21.2-4 to exclude versions 1.21.6 and above. Add Fabric 1.21.6-10

---
+++ Fabric-1.21.6-10/src/client/java/com/shaybox/durability101/Main.java
@@ -0,0 +1,8 @@
+package com.shaybox.durability101;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Main implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {}
+}
